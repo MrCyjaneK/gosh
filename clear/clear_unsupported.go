f660106389713556625dda73be97d154d16741f6
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-06-25 21:02:15 +0200

    clear anyway, even if unsupported

--- clear/clear_unsupported.go
+++ clear/clear_unsupported.go
@@ -8,6 +8,7 @@ import (
 )
 
 func Handle(cmd []string, STDIN *bufio.Reader, STDOUT *bufio.Writer, STDERR *bufio.Writer, CWD string, ENV map[string]string) uint8 {
-	STDOUT.Write([]byte("Unsupported!\n"))
-	return 1
+	STDOUT.Write([]byte("gosh: clear: Unsupported!\n"))
+	STDOUT.Write([]byte("\033[2J\033[H"))
+	return 0
 }
