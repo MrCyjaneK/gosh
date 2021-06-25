7f821f72411032e69fb93a2f32af36aa5e56a934
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-06-25 21:02:03 +0200

    updated debug

--- debug/main.go
+++ debug/main.go
@@ -3,11 +3,22 @@ package gosh_echo
 import (
 	"bufio"
 	"strconv"
+
+	"git.mrcyjanek.net/mrcyjanek/gosh/_core/h"
 )
 
+var flags map[string]string
+
 func Handle(cmd []string, STDIN *bufio.Reader, STDOUT *bufio.Writer, STDERR *bufio.Writer, CWD string, ENV map[string]string) uint8 {
+	flags, cmd = h.ParseFlags(cmd)
+
+	STDOUT.WriteString("CMD:\n")
 	for i := range cmd {
 		STDOUT.WriteString(" " + strconv.Itoa(i) + ". '" + cmd[i] + "'\n")
+	}
+	STDOUT.WriteString("FLAGS:\n")
+	for flag := range flags {
+		STDOUT.WriteString(" " + flag + ". '" + flags[flag] + "'\n")
 	}
 	return 0
 }
